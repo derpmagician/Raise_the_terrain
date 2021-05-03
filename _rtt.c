7eb718ac269508e1b3b80b3b56bc31a19ffbf8b5
Renzo <renzin@gmail.com>
2021-05-03 12:26:39 +0000

    first commit

---
+++ _rtt.c
@@ -0,0 +1,10 @@
+#include "terrain.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "=>: %s altitudes\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ rtt.h
@@ -0,0 +1,14 @@
+#ifndef _RTT_H_
+#define _RTT_H_
+
+#define PI 3.141592654
+#define SCR_W 640
+#define SCR_H 480
+#define ZSCALE 0.5
+#define INCLINATION 0.7
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <math.h>
+
+#endif /* _RTT_H_ */
